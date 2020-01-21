06c61b22a75ce5633bf61de761ee97d2eaebf83b
Nore5515 <Nore5515@gmail.com>
2020-01-21 11:22:48 -0600

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject clickMarker;
+    public List<UnitController> units;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        transform.position += move * speed * Time.deltaTime;
+
+        if (Input.GetMouseButton (0)){
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if ( Physics.Raycast (ray,out hit,100.0f)) {
+                //Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+                Debug.DrawRay(transform.position, ray.direction * 100.0f, Color.yellow);
+                Instantiate(clickMarker, hit.point, Quaternion.identity);
+                foreach (UnitController u in units){
+                  u.AddPoint(hit.point);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+
+    public float speed;
+    public List<Vector3> points;
+    int currentPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPoint = 0;
+        points = new List<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Check if the position of the cube and sphere are approximately equal.
+      if (points.Count > 0 && currentPoint < points.Count){
+
+        float step =  speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, points[currentPoint], step);
+        if (Vector3.Distance(transform.position, points[currentPoint]) < 0.001f)
+        {
+            // Swap the position of the cylinder.
+            currentPoint++;
+        }
+
+      }
+    }
+
+    public void AddPoint (Vector3 point)
+    {
+        points.Add(point);
+    }
+
+}
